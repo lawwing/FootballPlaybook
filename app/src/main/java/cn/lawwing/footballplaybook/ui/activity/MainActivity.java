9182ad9003a337c67a05b23a0aa54b8da106220f
lzr <123456>
2017-12-11 20:42:12 +0800

    新增了MVP的项目结构目录

--- app/src/main/java/cn/lawwing/footballplaybook/ui/activity/MainActivity.java
+++ app/src/main/java/cn/lawwing/footballplaybook/ui/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package cn.lawwing.footballplaybook;
+package cn.lawwing.footballplaybook.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import cn.lawwing.footballplaybook.R;
 
 public class MainActivity extends AppCompatActivity
 {
